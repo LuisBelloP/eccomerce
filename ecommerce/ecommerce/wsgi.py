f82c3afed2606c1f926160040861f24078e3cf3d
Luis <luisernestoperezbello@gmail.com>
2023-10-24 14:00:19 -0600

    ready3

--- ecommerce/ecommerce/wsgi.py
+++ ecommerce/ecommerce/wsgi.py
@@ -4,6 +4,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_app.settings')
 
 application = get_wsgi_application()
